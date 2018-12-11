e64293c758e9ccaa1d00ba40c301cd5b0931d660
Milosz Skaza <milosz.skaza@gmail.com>
2018-12-11 22:53:35 +0100

    Fixes #1

--- src/main/java/com/example/demo/service/BookService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.model.Book;
-
-public interface BookService {
-    public void update(Book book);
-    public Book sendBook(Long id);
-
-}
